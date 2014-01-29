00cc991195003d469a4cb814a9a4fe61aa7f9b5d
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-01-29 19:39:08 +0000

    pause between senign each file as not to choke the fs or s3

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ var fs = require('fs');
 var knox = require('knox');
 
 module.exports = function (options) {
-    return es.map(function (file, cb) {
-        var isFile = fs.lstatSync(file.path).isFile();
+  var client = knox.createClient(options);
+  var waitTime = 0;
+  return es.mapSync(function (file, cb) {
+      var isFile = fs.lstatSync(file.path).isFile();
+      if (!isFile) { return false; }
 
-        if (!isFile) {
-            return false;
-        }
+      var uploadPath = file.path.replace(file.base, '');
+      var headers = { 'x-amz-acl': 'public-read' };
 
-        var uploadPath = file.path.replace(file.base, '');
-        var client = knox.createClient(options);
-
-        var headers = {
-            'x-amz-acl': 'public-read'
-        };
-
+      setTimeout(function() {
         client.putFile(file.path, uploadPath, headers, function(err, res) {
             if (err || res.statusCode !== 200) {
-                console.log("Error Uploading" + res.req.path);
+                console.log("Error uploading " + file.path);
             } else {
-                console.log("Uploaded " + res.req.path);
+                console.log("Uploaded " + file.path);
+                res.resume();
             }
+
         });
-    });
+      }, waitTime);
+
+      waitTime += 50;
+  });
 };
