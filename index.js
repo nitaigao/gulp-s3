a8e3b89a1b38d65e1fcb96130bf7f72300e3831b
Joshua Bellamy-Henn <smysnk@gmail.com>
2014-03-04 16:34:43 -0700

    feat(options): gzippedOnly- Will only upload gzipped files, removes .gz suffix on the destination and sets the appropriate headers

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ module.exports = function (aws, options) {
   options = options || {};
   
   if (!options.delay) { options.delay = 0; }
-
+  
   var client = knox.createClient(aws);
   var waitTime = 0;
+  var regex = /\.([a-z]{2,4})\.gz$/i;
 
   return es.mapSync(function (file, cb) {
       var isFile = fs.lstatSync(file.path).isFile();
+
+      // Verify this is a file and it doesn't matche our filter
       if (!isFile) { return false; }
 
       var uploadPath = file.path.replace(file.base, options.uploadPath || '');
+
       var headers = { 'x-amz-acl': 'public-read' };
       if (options.headers) {
           for (var key in options.headers) {
@@ -25,12 +29,53 @@ module.exports = function (aws, options) {
           }
       }
 
+      var matches;
+      if (options.gzippedOnly && !/\.gz$/gi.test(file.path)) { 
+        // If we have gzip enabled, ignore non-gzipped files
+        return false; 
+      } else if (options.gzippedOnly && (matches = regex.exec(file.path))) {
+        // Handle uploading of gzipped files, set headers + rename extension
+
+        var suffix = matches[1];
+        var encoding = 'plain';
+        
+        switch (suffix) {
+            case 'js':
+                suffix = 'javascript';
+                encoding = 'text';
+                break;
+
+            case 'css':
+            case 'html': 
+                encoding = 'text';
+                break;        
+
+            case 'jpg':
+                encode = 'image';
+                suffix = 'jpeg';
+                break;
+
+            case 'png':
+            case 'bmp':
+            case 'gif':
+                encoding = 'image';
+                break;
+
+            default:
+                suffix = 'text'; // If can't find a suitable encoding, set to text/plain
+        }
+
+        headers['Content-Encoding'] = 'gzip';
+        headers['Content-Type'] = encoding + '/' + suffix;
+        uploadPath = uploadPath.substring(0, uploadPath.length - 3);
+      }
+
       setTimeout(function() {
         client.putFile(file.path, uploadPath, headers, function(err, res) {
             if (err || res.statusCode !== 200) {
-                gutil.log(gutil.colors.red('[FAILED]', file.path));
+                gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
             } else {
-                gutil.log(gutil.colors.green('[SUCCESS]', file.path));
+                gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
                 res.resume();
             }
         });
