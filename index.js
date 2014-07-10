f4898f0c9bf6d5d7e3879e9f8a41c1c5d4f1f48d
Jamie Popkin <popkinj@littleearth.ca>
2014-07-09 17:15:55 -0700

    Fixed the indenting to match the rest of the doc.

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ module.exports = function (aws, options) {
         }
       }
 
-		headers['Content-Length'] = file.contents.toString().length;
+      headers['Content-Length'] = file.contents.toString().length;
 
 
       client.putBuffer(file.contents, uploadPath, headers, function(err, res) {
