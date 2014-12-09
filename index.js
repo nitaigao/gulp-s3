edf15e2a20b10dd70abfc27027e6984068af4d9f
ken210 <ken@loggi.com>
2014-12-09 20:12:33 -0200

    typo

--- index.js
+++ index.js
@@ -48,15 +48,15 @@ module.exports = function (aws, options) {
       }
 
       headers['Content-Length'] = file.stat.size;
-      
+
       var retries = 0;
 
       function doPut (err, res) {
         if (err || res.statusCode !== 200) {
           gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
           gutil.log(gutil.colors.yellow('[RETRYING]', file.path + " -> " + uploadPath));
-          if (options.retries && i < options.retries) {
-            i++;
+          if (options.retries && retries < options.retries) {
+            retries++;
             client.putBuffer(file.contents, uploadPath, headers, doPut);
           } else {
             gutil.log(gutil.colors.pink('[MAX RETRIES]', file.path + " -> " + uploadPath));
