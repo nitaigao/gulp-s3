f839fa50924e979464074d722a709016f195236e
Michael Pelz-Sherman <mpelzsherman@gmail.com>
2015-09-16 17:12:30 -0400

    [MPS] [103525708] added colors to debug statements

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ module.exports = function (aws, options) {
         if (err || res.statusCode !== 200) {
           gutil.log(gutil.colors.red('[FAILED]', file.path + " -> " + uploadPath));
           if (options.dieOnError) {
-            gutil.log("exiting with status -1");
+            gutil.log(gutil.colors.red("exiting with status -1"));
             process.exit.bind(process, -1);
           } else {
-            gutil.log("continuing to publish...");
+            gutil.log(gutil.colors.green("continuing to publish..."));
           }
         } else {
           gutil.log(gutil.colors.green('[SUCCESS]', file.path + " -> " + uploadPath));
