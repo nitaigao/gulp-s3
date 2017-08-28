c92086b590a1ee8928863e4cd476eb88ffe55790
cyruskazemi <cyrusk@porch.com>
2017-08-28 15:33:04 -0700

    Allow numbers in gzip file extensions

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function (aws, options) {
 
   var client = knox.createClient(aws);
   var waitTime = 0;
-  var regexGzip = /\.([a-z]{2,})\.gz$/i;
+  var regexGzip = /\.([a-z0-9]{2,})\.gz$/i;
   var regexGeneral = /\.([a-z0-9]{2,})$/i;
 
   return es.map(function (file, finished) {
